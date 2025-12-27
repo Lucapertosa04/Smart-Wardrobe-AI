6d4c37083f46ce8ba357bab99c9c6f5d91b095a2
Flaviomonaco04 <f.monaco4@studenti.poliba.it>
2025-12-27 18:22:41 +0100

    Funzione che invia dati inseriti dall'utente

--- Smart Wardrobe AI/frontend/src/api/wardrobeApi.js
+++ Smart Wardrobe AI/frontend/src/api/wardrobeApi.js
@@ -1,1 +1,35 @@
-//chiamata Api backend
+//Funzione che invia al backend i dati inseriti dall'utente
+
+export async function analyzeGarment(UserInput) {
+    const API_URL = "http://127.0.0.1:5000/api/wardrobe/analyze";
+
+    try{
+        const response = await fetch(API_URL, {
+            method: "POST",
+            headrs: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(UserInput)
+        });
+
+        if(!response.ok){
+            const errorData = await response.json();
+
+            throw new Error(errorData.error || "Errore nella richiesta");
+        }
+
+        const data = await response.json();
+
+        return data;
+    }catch(error){
+        //stampa errore in console per facilitare debug
+        console.error("Errore Wardrobe API: ", error);
+
+        return{
+            status: "error",
+            message: error.message
+        }
+    }
+}
+
+//Ponte tra frontend e backend per quanto riguarda l'analisi del capo d'abbigliamento
