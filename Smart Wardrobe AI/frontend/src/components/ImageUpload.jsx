4c170e15413260c99dc3b6f5f48e665bd1486072
Flaviomonaco04 <f.monaco4@studenti.poliba.it>
2025-12-27 19:54:08 +0100

    Aggiunto ImageUpload

--- Smart Wardrobe AI/frontend/src/components/ImageUpload.jsx
+++ Smart Wardrobe AI/frontend/src/components/ImageUpload.jsx
@@ -1,1 +1,25 @@
 //Upload immagini
+
+
+export default function ImageUpload({ onFileSelect }) {
+  function handleChange(event) {
+    const file = event.target.files[0];
+    if (file) {
+      onFileSelect(file);
+    }
+  }
+
+  return (
+    <div className="upload-box">
+      <label className="upload-label">
+        Trascina un'immagine oppure clicca per selezionarla
+        <input
+          type="file"
+          accept="image/*"
+          hidden
+          onChange={handleChange}
+        />
+      </label>
+    </div>
+  );
+}
